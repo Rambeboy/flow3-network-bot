0404fe363aebef6d6be0dc4b4f8fb9c13b209aca
Nofan Rambe <kingindoslayer@gmail.com>
2025-03-20 21:58:30 +0700

    Update accounts_tmp.js

--- accounts/accounts_tmp.js
+++ accounts/accounts_tmp.js
@@ -1,1 +1,11 @@
+/**
+ * Private key list file
+ * Write your private key here like this
+ * export const privateKey = [
+ *   "PK1",
+ *   "PK2",
+ *   "PK3",
+ * ];
+ */
+
 export const privateKey = [];
